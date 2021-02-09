d0cdf85de895f90ae5da92eed715b467a73b0b03
sizaif <Sizaif2000@gmail.com>
2021-02-09 22:35:26 +0800

    添加PyTorchLib依赖

--- sealtest.cpp
+++ sealtest.cpp
@@ -2,7 +2,8 @@
 #include "seal/seal.h"
 #include "examples.h"
 #include <iostream>
-
+#include <torch/torch.h>
+#include <torch/script.h>
 
 using namespace std;
 using namespace seal;
@@ -13,10 +14,21 @@ int main()
     //example_encoders();
     //example_levels();
     //定义加密参数
-    void test1(); // 声明一下test1函数，源代码在demotest里
-    test1();
+    //void test1(); // 声明一下test1函数，源代码在demotest里
+    //test1();
     //example_ckks_basics();
     //example_rotation();
+    
+    torch::Tensor tensor = torch::rand({ 5,3 });
+    std::cout << tensor << std::endl;
+    /*
+    torch::Tensor tensor = torch::rand({ 5,3 });
+    std::cout << tensor << std::endl;
+
+    return EXIT_SUCCESS;
+    */
+
+
     return 0;
 }
 
